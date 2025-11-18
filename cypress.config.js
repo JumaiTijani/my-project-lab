004ae37e9d3df570ff7c2eb848056ef262349bfe
Jumai Tijani <tijanij25@gmail.com>
2025-11-18 06:28:50 +0100

    adding new tab handling

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     //baseUrl: 'https://staging.trymima.com',
-    baseUrl: "https://www.dummyticket.com/",
+    //baseUrl: "https://kitchen.applitools.com/",
+    //baseUrl: "https://www.lambdatest.com/selenium-playground/",
+    baseUrl: "https://the-internet.herokuapp.com/",
+    //baseUrl: "https://www.dummyticket.com/",
     defaultCommandTimeout: 12000,
     viewportHeight: 960,
     viewportWidth: 1530,
